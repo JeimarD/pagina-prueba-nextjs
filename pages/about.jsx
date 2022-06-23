6a50deccd4b82c9b1091242a9a7e6f6a3bff634c
JeimarD <jeimar1512@gmail.com>
2022-06-23 10:21:16 -0400

    Mi pagina con Next.js probando

---
+++ pages/about.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function about() {
+  return (
+    <Layout
+      title="About | Next.js"
+      description="Agregue una descripcion al about"
+    >
+      <h1>About.js</h1>
+    </Layout>
+  );
+}
---
+++ pages/contact.jsx
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+export default function contact() {
+  return (
+    <Layout
+      title="Contact | Next.js"
+      description="Agregue una descripcion al contactoo"
+    >
+      <h1>El contacto</h1>
+    </Layout>
+  );
+}
